a58214831b4308907092a3f8c19927438d3affe3
eyere emagbetere <eeyere@gmail.com>
2022-08-12 10:06:17 +0100

    First codewars practice commit kyu 6

---
+++ Codewars_KYU6_practice.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul 28 08:52:46 2022
+
+@author: HP
+"""
+#This fike conatins a function that can determine the word with the max count
+# in a sentence 
+#shown as a string
+def high(x):
+    # Code here
+    x=x.split()
+    letters =' abcdefghijklmnopqrstuvwxyz' 
+    word_value=[]
+    for index, word in enumerate(x):
+        word_value.append(sum([letters.index(i) for i in word.lower()]))
+        print(word_value)
+        
+    return x[word_value.index(max(word_value))]
+
+a=("aaa b")
+
+print(high(a))
+            
