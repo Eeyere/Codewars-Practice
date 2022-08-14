225ada157c3c02d7c6c93ce13cb150b7a7391796
eyere emagbetere <eeyere@gmail.com>
2022-08-14 18:15:14 +0100

    Other better functions added

--- convert_tupple_of_3_ele_to_hex.py
+++ convert_tupple_of_3_ele_to_hex.py
@@ -30,8 +30,24 @@ def rgb(r, g, b):
 
 
 print(rgb(67,255,-3))        
+
+#Other shorter codes shown below:
+
+def rgb2(r, g, b):
+    round = lambda x: min(255, max(x, 0))
+    return ("{:02X}" * 3).format(round(r), round(g), round(b))
             
+print(rgb2(67,255,-3))               
             
-            
+def rgb3(*args):
+    return ''.join(map(lambda x: '{:02X}'.format(min(max(0, x), 255)), args));
     
-    
+print(rgb3(67,255,-3))    
+
+
+def rgb4(r, g, b):
+    def get_hex(s):
+        if s > 255: s = 255
+        if s < 0: s = 0
+        return hex(s)[2:].upper() if len(hex(s)[2:]) > 1 else "0" + hex(s)[2:]
+    return get_hex(r) + get_hex(g) + get_hex(b)
