b040a1d6de6528098eb76b8682524f11ed28fddd
Eyere Emagbetere <eeyere@gmail.com>
2022-08-24 10:44:02 +0100

    other codes added the the file

--- cakes_num.py
+++ cakes_num.py
@@ -4,6 +4,10 @@ Created on Wed Aug 24 09:52:10 2022
 
 @author: HP
 """
+
+# Dictionaries representing objects were made to divide between recipes and what is available
+# the results appended to a list and the minimum is returned 
+
 r={'flour': 500, 'sugar': 200, 'eggs': 1}
 a={'flour': 1200, 'sugar': 1200, 'eggs': 5, 'milk': 200}
 
@@ -18,3 +22,15 @@ def cakes(recipe, available):
     return int(min(aa))
 
 print(cakes(r,a))
+
+#Other forms of the code is shown below
+
+def cakes2(recipe, available):
+    try:
+        return min([available[a]/recipe[a] for a in recipe])
+    except:
+        return 0
+    
+def cakes(recipe, available):
+
+    return min([available[i]//recipe[i] if i in available else 0 for i in recipe])
